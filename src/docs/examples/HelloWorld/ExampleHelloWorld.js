53be5b5074462e733c46630dbf03871dc5e073fc
Rob Byrne <rob@leanrob.com>
2018-02-28 01:55:35 -0800

    Adding webpack alias for component paths
    - added alias ‘lean-react’
    - used alias in example hello world component and it works

--- src/docs/examples/HelloWorld/ExampleHelloWorld.js
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HelloWorld from '../../../components/HelloWorld/HelloWorld';
+import HelloWorld from 'lean-react/HelloWorld';
 
 /** With a custom message: */
 export default function ExampleHelloWorld() {
