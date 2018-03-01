3af9dbedca362720acd94470e820a4072359a501
leanrob <rob@leanrob.com>
2018-03-01 00:48:41 -0800

    Add label component Atom
    —> single Atom component made for a label

---
+++ src/docs/examples/HelloWorld/ExampleRobHello.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from 'lean-react/HelloWorld';
+
+/** With a custom message: */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="Hello Rob!" />
+}
---
+++ src/docs/examples/Label/ExampleLabel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Label from 'lean-react/Label';
+
+const ExampleLabel = () => {
+  return (
+    <Label htmlFor="test" label="text" />
+  )
+}
+
+export default ExampleLabel;
