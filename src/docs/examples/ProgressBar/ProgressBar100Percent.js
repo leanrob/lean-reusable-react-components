ad66a22cc752dfc6a216b21892ab14b83425c4be
leanrob <rob@leanrob.com>
2018-03-01 17:33:54 -0800

    Adding testing frameworks to the project and sample test
    - react-test-renderer enzyme react-addons-test-utils
    - Added sample test
    - Next is to test and add a snapshot test example

---
+++ src/docs/examples/ProgressBar/ProgressBar100Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'lean-react/ProgressBar';
+
+/** 100% progress with 20px height */
+export default function Example100PercentWithCustomHeight() {
+  return <ProgressBar percent={100} width={150} height={20} />
+}
